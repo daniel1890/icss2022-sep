cde21cc229dec63097283d080576ded7ceeccf6d
Daniël <d.roth@student.han.nl>
2023-10-19 20:12:10 +0200

    tests queue & stack done

--- startcode/src/test/java/nl/han/ica/datastructures/HANQueueTest.java
+++ startcode/src/test/java/nl/han/ica/datastructures/HANQueueTest.java
@@ -2,16 +2,76 @@ package nl.han.ica.datastructures;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HANQueueTest {
+    private IHANQueue<Integer> sut;
 
     @BeforeEach
     void setUp() {
+        sut = new HANQueue<>();
     }
 
     @AfterEach
     void tearDown() {
+    }
+
+    @Test
+    void enqueueAddsElementToBackOfQueue() {
+        // Arrange
+        int frontElement = 1;
+        int backElement = 2;
+
+        // Act
+        this.sut.enqueue(frontElement);
+        this.sut.enqueue(backElement);
+
+        // Assert
+        assertEquals(frontElement, this.sut.peek());
+    }
+
+    @Test
+    void dequeueRemovesElementFirstInQueue() {
+        // Arrange
+        int frontElement = 1;
+        int backElement = 2;
+
+        // Act
+        this.sut.enqueue(frontElement);
+        this.sut.enqueue(backElement);
+
+        // Assert
+        assertEquals(frontElement, this.sut.dequeue());
+        assertEquals(backElement, this.sut.peek());
+    }
+
+    @Test
+    void clearDeletesEntireList() {
+        // Arrange
+        Integer expectedLengthBeforeClear = 1;
+        Integer expectedLength = 0;
+        sut.enqueue(1);
+
+        // Act
+        assertEquals(1, sut.getSize());
+        sut.clear();
+
+        // Assert
+        assertEquals(expectedLength, sut.getSize());
+    }
+
+    @Test
+    void isEmpty() {
+        // Arrange
+        sut.enqueue(1);
+
+        // Act
+        assertFalse(sut.isEmpty());
+        sut.clear();
+
+        // Assert
+        assertTrue(sut.isEmpty());
     }
 }
--- startcode/src/test/java/nl/han/ica/datastructures/HANStackTest.java
+++ startcode/src/test/java/nl/han/ica/datastructures/HANStackTest.java
@@ -2,16 +2,47 @@ package nl.han.ica.datastructures;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HANStackTest {
+    IHANStack<Integer> sut;
 
     @BeforeEach
     void setUp() {
+        sut = new HANStack<>();
     }
 
     @AfterEach
     void tearDown() {
     }
+
+    @Test
+    void pushElementOnTopOfStack() {
+        // Arrange
+        int element = 1;
+
+        // Act
+        sut.push(element);
+
+        // Assert
+        assertEquals(element, sut.peek());
+    }
+
+    @Test
+    void popRemovesElementOnTopOfStack() {
+        // Arrange
+        int bottomElement = 1;
+        int topElement = 2;
+        sut.push(bottomElement);
+        sut.push(topElement);
+
+        // Act
+        sut.pop();
+
+        // Assert
+        assertEquals(bottomElement, sut.peek());
+    }
+
 }
